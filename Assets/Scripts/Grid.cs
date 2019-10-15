95da028f9b143956294e27d481497ec5bc89cc46
EGLIK <eglik@odin.vision>
2019-10-15 12:25:00 +0900

    test: bilinear

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -29,6 +29,10 @@ public class Grid : MonoBehaviour
             case InterpolationType.Nearest:
                 Nearest(texture);
                 break;
+
+            case InterpolationType.Bilinear:
+                Bilinear(texture);
+                break;
         }
         texture.Apply();
         GetComponent<RawImage>().texture = texture;
@@ -41,6 +45,35 @@ public class Grid : MonoBehaviour
             for (int k = 0; k < height; k++)
             {
                 texture.SetPixel(i, k, colorGradient.GetColor(pivot[Mathf.FloorToInt(k / size)][Mathf.FloorToInt(i / size)]));
+            }
+        }
+    }
+
+    private void Bilinear(Texture2D texture)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int k = 0; k < height; k++)
+            {
+                var dx = Mathf.Repeat(i / size, 1);
+                var dy = Mathf.Repeat(k / size, 1);
+
+                var x = Mathf.FloorToInt(i / size);
+                var y = Mathf.FloorToInt(k / size);
+
+                try
+                {
+
+                    var t0 = Mathf.Lerp(pivot[y][x], pivot[y][x + 1], dx);
+                    var t1 = Mathf.Lerp(pivot[y + 1][x], pivot[y + 1][x + 1], dx);
+
+                    var t2 = Mathf.Lerp(t0, t1, dy);
+                    texture.SetPixel(i, k, colorGradient.GetColor(t0));
+                }
+                catch
+                {
+
+                }
             }
         }
     }
